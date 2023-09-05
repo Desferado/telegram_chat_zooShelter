11ad5aaa9ab4fdf953664d8e21472bc90081ec71
Denis_vrn <denisfedorov50@gmail.com>
2023-09-05 19:21:37 +0300

    Start project

---
+++ src/main/java/pro/sky/telegram_chat_zooShelter/TelegramChatZooShelterApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegram_chat_zooShelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramChatZooShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramChatZooShelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/telegram_chat_zooShelter/TelegramChatZooShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegram_chat_zooShelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramChatZooShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
