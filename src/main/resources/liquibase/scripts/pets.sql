c51f5e2383dbb989126dfebf3b245214a5694e27
Марат <120682382+gtMarat@users.noreply.github.com>
2023-09-08 17:50:06 +0300

    Добавил таблицу клиентов

---
+++ src/main/resources/liquibase/scripts/pets.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+--changeset marat:1
+
+create TABLE customer (  -- Таблица: Пользователь
+ id BIGINT,          -- уникальный id
+chat_id BIGINT,    -- id Telegram чата
+surname VARCHAR(25) , -- фамилия
+name VARCHAR(25),    -- имя
+second_name VARCHAR(25), -- отчество
+phone VARCHAR(12),       -- тлф формата +70000000000
+address TEXT             -- адрес
+);
