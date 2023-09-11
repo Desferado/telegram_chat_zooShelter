2feea725fd641e408ffe62423fccbc511a5636d6
Марат <120682382+gtMarat@users.noreply.github.com>
2023-09-11 11:30:11 +0300

    Добавил в Listener , кнопки выбора меню

--- src/main/java/pro/sky/telegram_chat_zooShelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegram_chat_zooShelter/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegram_chat_zooShelter.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
@@ -31,12 +33,77 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
-            if (update.message().text().equals("/start")) {
-                Long chatId = update.message().chat().id();
-                SendMessage sendMess = new SendMessage(chatId, "Привет!!! Хочешь взять собаку из приюта");
-                SendResponse response = telegramBot.execute(sendMess);
+            if (update.message() != null) {
+                if (update.message().text().equals("/start")) {
+                    Long chatId = update.message().chat().id();
+                    responseOnCommandStart(chatId);
+                }
+            }
+
+            else if (update.callbackQuery() != null) {
+                if (update.callbackQuery().data().equals("ONE")) {
+                    Long chatId = update.callbackQuery().message().chat().id();
+                    responseOnCommandInfoShelter(chatId);
+                }
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+    private void responseOnCommandStart(long chatId) {
+
+        SendMessage sendMess = new SendMessage(chatId, "Привет друг!" +
+                "\nЯ бот-помощник и помогу познакомится " +
+                "\nс приютом и питомцами.");
+        sendMess.replyMarkup(preparekeyboardStart());
+        SendResponse response = telegramBot.execute(sendMess);
+    }
+    private void responseOnCommandInfoShelter(long chatId) {
+
+        SendMessage sendMess = new SendMessage(chatId, "Выбери то,что тебя интересует");
+        sendMess.replyMarkup(preparekeyboardInfoShelter());
+        SendResponse response = telegramBot.execute(sendMess);
+    }
+    /* метод создает инлайн клавиатуру после отправки команды Start
+    @return клавиатура подсообщением
+     */
+
+    private InlineKeyboardMarkup preparekeyboardStart() {
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Инфо о приюте");
+        InlineKeyboardButton button2 = new InlineKeyboardButton("Как взять питомца");
+        InlineKeyboardButton button3 = new InlineKeyboardButton("Отчет о питомце");
+        InlineKeyboardButton button4 = new InlineKeyboardButton("Позвать волонтера");
+
+        button1.callbackData("ONE");
+        button2.callbackData("TWO");
+        button3.callbackData("THREE");
+        button4.callbackData("FORTH");
+
+        markupInline.addRow(button1, button2);
+        markupInline.addRow(button3, button4);
+
+        return markupInline;
+
+    }
+   /* метод создает инлайн клавиатуру после отправки команды для получения информации о приюте
+     @return клавиатура под сообщением
+    */
+    private InlineKeyboardMarkup preparekeyboardInfoShelter() {
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton("О приюте");
+        InlineKeyboardButton button2 = new InlineKeyboardButton("График работы, адрес");
+        InlineKeyboardButton button3 = new InlineKeyboardButton("Правила приюта");
+        InlineKeyboardButton button4 = new InlineKeyboardButton("Оставить контактные данные");
+
+        button1.callbackData("INFO");
+        button2.callbackData("GRAPHIC");
+        button3.callbackData("RULES");
+        button4.callbackData("CONTACTS");
+
+        markupInline.addRow(button1, button2);
+        markupInline.addRow(button3, button4);
+        return markupInline;
     }
 }
