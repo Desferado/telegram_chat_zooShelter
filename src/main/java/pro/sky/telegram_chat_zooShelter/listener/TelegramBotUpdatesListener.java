9d15a2e80d01a41c4d56b1fdfcf5c45ac9dbdc05
Denis_vrn <denisfedorov50@gmail.com>
2023-09-11 20:06:47 +0300

    Добавил ответ бота на выбор приюта.

--- src/main/java/pro/sky/telegram_chat_zooShelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegram_chat_zooShelter/listener/TelegramBotUpdatesListener.java
@@ -8,18 +8,25 @@ import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegram_chat_zooShelter.component.SendNotificationTask;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
     private TelegramBot telegramBot;
+    private  final SendNotificationTask sendNotificationTask;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, SendNotificationTask sendNotificationTask) {
+        this.telegramBot = telegramBot;
+        this.sendNotificationTask = sendNotificationTask;
+    }
 
     @PostConstruct
     public void init() {
@@ -30,12 +37,17 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            String text = update.message().text();
+            Long chatId = update.message().chat().id();
             // Process your updates here
-            if (update.message().text().equals("/start")) {
-                Long chatId = update.message().chat().id();
-                SendMessage sendMess = new SendMessage(chatId, "Привет!!! Хочешь взять собаку из приюта");
-                SendResponse response = telegramBot.execute(sendMess);
+            if ("/start".equals(text)) {
+                sendNotificationTask.sendMessage(chatId, "Привет!!! Выберите, пожалуйста, приют: Кошки или Собаки.");
+            } else if ("Кошки".equals(text)){
+                sendNotificationTask.sendMessage(chatId, "Вы выбрали приют кошек");
+            } else if ("Собаки".equals(text)) {
+                sendNotificationTask.sendMessage(chatId, "Вы выбрали приют собак");
             }
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
