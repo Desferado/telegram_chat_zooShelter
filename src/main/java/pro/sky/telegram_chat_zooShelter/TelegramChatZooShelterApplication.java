960a6c16ba1a866f7c4395e3b7caac81fb1422bb
Denis_vrn <denisfedorov50@gmail.com>
2023-09-16 00:24:33 +0300

    Добавил API документацию в контроллер клиентов.

--- src/main/java/pro/sky/telegram_chat_zooShelter/TelegramChatZooShelterApplication.java
+++ src/main/java/pro/sky/telegram_chat_zooShelter/TelegramChatZooShelterApplication.java
@@ -1,9 +1,11 @@
 package pro.sky.telegram_chat_zooShelter;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class TelegramChatZooShelterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/pro/sky/telegram_chat_zooShelter/controller/CustomerController.java
+++ src/main/java/pro/sky/telegram_chat_zooShelter/controller/CustomerController.java
@@ -1,6 +1,14 @@
 package pro.sky.telegram_chat_zooShelter.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.telegram_chat_zooShelter.model.Customer;
@@ -16,14 +24,34 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
-
+@Operation(
+        summary = "Получение списка всех клиентов",
+    responses = {
+        @ApiResponse(
+                responseCode = "200",
+                description = "Получение списка всех клиентов",
+                content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                array = @ArraySchema (schema = @Schema(implementation = Customer.class))
+                )
+        )
+})
     @GetMapping("/get-customers")
     public ResponseEntity <List<Customer>> getAllCustomers() {
         return ResponseEntity.ok(customerService.getCustomers());
     }
-
+    @Operation(
+            summary = "Поиск клиента по id",
+            responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Поиск клиента по id",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Customer.class)
+                    )
+            )
+    })
     @GetMapping("{id}")
-    public ResponseEntity <Customer> getCustomerById(@PathVariable Long id) {
+    public ResponseEntity <Customer> getCustomerById(@Parameter (description = "Клиент с данным id") @PathVariable Long id) {
         Customer customer = customerService.findCustomerById(id);
         if (customer == null) {
             return ResponseEntity.notFound().build();
@@ -31,12 +59,28 @@ public class CustomerController {
             return ResponseEntity.ok(customer);
         }
     }
-
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Заведение клиента в базу",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation =  Customer.class)
+                    )
+            )
+    })
     @PostMapping
     public ResponseEntity <Customer> createCustomer(@RequestBody Customer customer) {
         return ResponseEntity.ok(customerService.createCustomer(customer));
     }
-
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Изменение клиента",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation =  Customer.class)
+                    )
+            )
+    })
     @PutMapping
     public ResponseEntity <Customer> updateCustomer(@RequestBody Customer newCustomer) {
         Customer customer = customerService.updateCustomer(newCustomer);
@@ -46,7 +90,13 @@ public class CustomerController {
             return ResponseEntity.ok(customer);
         }
     }
-
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Удаление клиента из базы",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+            )
+    })
     @DeleteMapping("{id}")
     public ResponseEntity <Customer> removeCustomer(@PathVariable Long id) {
         Customer customer = customerService.deleteCustomerById(id);
