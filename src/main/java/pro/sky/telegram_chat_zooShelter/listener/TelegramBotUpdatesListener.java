8c7f72a2028b0725759b85bf43834ad362556f78
Denis_vrn <denisfedorov50@gmail.com>
2023-11-08 00:50:23 +0300

    Пытаюсь сохранить фото из телеграмм бота в локальной папке

--- src/main/java/pro/sky/telegram_chat_zooShelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegram_chat_zooShelter/listener/TelegramBotUpdatesListener.java
@@ -13,17 +13,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.telegram.telegrambots.meta.api.methods.*;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import pro.sky.telegram_chat_zooShelter.component.ResponseOnCommand;
 import pro.sky.telegram_chat_zooShelter.component.SendMessages;
 import pro.sky.telegram_chat_zooShelter.model.Customer;
 import pro.sky.telegram_chat_zooShelter.services.CustomerService;
 import pro.sky.telegram_chat_zooShelter.services.PhotoPetService;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -31,108 +29,113 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
+import static org.apache.commons.io.FileUtils.getFile;
 import static pro.sky.telegram_chat_zooShelter.constants.Constants.*;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-    @Value("${path.to.avatars.folder}")
-    private String coversDir;
+
     @Autowired
     private TelegramBot telegramBot;
     private User telegramCustomer;
     private final CustomerService customer;
     private final SendMessages sendMessages;
     private final ResponseOnCommand responseOnCommand;
-    private final PhotoPetService photoPetService;
 
     private String nameCustomer;
     private String tlText;
-    @Value("${telegram.bot.token}")
-    private String token;
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot, CustomerService customer,
-                                      SendMessages sendMessages, ResponseOnCommand responseOnCommand,
-                                      PhotoPetService photoPetService) {
+                                      SendMessages sendMessages, ResponseOnCommand responseOnCommand) {
         this.telegramBot = telegramBot;
         this.customer = customer;
         this.sendMessages = sendMessages;
         this.responseOnCommand = responseOnCommand;
-        this.photoPetService = photoPetService;
     }
 
     @PostConstruct
     public void init() {
         telegramBot.setUpdatesListener(this);
     }
-
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            if (update.message() != null && update.message().text() != null) {
-                telegramCustomer = update.message().from();
-                tlText = update.message().text();
-                nameCustomer = telegramCustomer.firstName();
-                if (tlText.equals("/start")) {
-                    Long chatId = update.message().chat().id();
-                    responseOnCommand.responseOnCommandStart(chatId, nameCustomer);
-                    if (customer.findCustomerByChatId(chatId) == null) {
-                        customer.createCustomer(new Customer(
-                                telegramCustomer.id(),
-                                chatId,
-                                telegramCustomer.lastName(),
-                                telegramCustomer.firstName(),
-                                telegramCustomer.username(),
-                                null,
-                                null,
-                                null,
-                                null,
-                                null
-                        ));
+            if (update.message() != null){
+                if(update.message().text() != null) {
+                    telegramCustomer = update.message().from();
+                    tlText = update.message().text();
+                    nameCustomer = telegramCustomer.firstName();
+                    if (tlText.equals("/start")) {
+                        Long chatId = update.message().chat().id();
+                        responseOnCommand.responseOnCommandStart(chatId, nameCustomer);
+                        if (customer.findCustomerByChatId(chatId) == null) {
+                            customer.createCustomer(new Customer(
+                                    telegramCustomer.id(),
+                                    chatId,
+                                    telegramCustomer.lastName(),
+                                    telegramCustomer.firstName(),
+                                    telegramCustomer.username(),
+                                    null,
+                                    null,
+                                    null,
+                                    null,
+                                    null
+                            ));
+                        }
+                    } else if (tlText.toLowerCase().contains("отчет")) {
+                        sendMessages.sendMessage(1284536796L, LocalDate.now() + " - Клиент "
+                                + nameCustomer
+                                + " id" + telegramCustomer.id()
+                                + " прислал отчет: " + tlText);
+                    } else if (tlText.toLowerCase().contains("контакт")) {
+                        sendMessages.sendMessage(1284536796L, "Клиент " + nameCustomer
+                                + " прислал свои контактные данные для связи: " + tlText);
                     }
-                } else if (tlText.toLowerCase().contains("отчет")) {
-                    sendMessages.sendMessage(1284536796L, LocalDate.now() + " - Клиент "
-                            + nameCustomer
-                            + " id" + telegramCustomer.id()
-                            + " прислал отчет: " + tlText);
-                } else if (tlText.toLowerCase().contains("контакт")) {
-                    sendMessages.sendMessage(1284536796L, "Клиент " + nameCustomer
-                            + " прислал свои контактные данные для связи: " + tlText);
-                }
-            } else if (update.message() != null && update.message().photo() != null) {
-                telegramCustomer = update.message().from();
-                nameCustomer = telegramCustomer.firstName();
-                Long chatId = update.message().chat().id();
-                long petsId = customer.findCustomerByChatId(chatId).getPets().getId();
-                PhotoSize[] photoSizes = update.message().photo();
-                List<PhotoSize> photos = Arrays.stream(photoSizes).toList();
-                PhotoSize photoSize = Objects.requireNonNull(photos.stream()
-                        .max(Comparator.comparing(PhotoSize::fileSize))
-                        .orElse(null));
-                String f_id = photoSize.fileId();
-                String fileT = photoSize.toString();
-                String nameFile = LocalDate.now() + " Фото питомца с id = " + petsId
-                        + ".";
-                sendMessages.sendMessage(1284536796L, "Клиент " + nameCustomer +
-                        " id = "
-                        + " прислал фото " + nameFile
-                );
-                try {
-                    GetFile getFileMethod = new GetFile(f_id);
-                   File file = execute(getFileMethod); // Выполнение запроса на получение информации о файле
-                    String filePath = file.getFilePath(); // Получаем путь к файлу
-                    String originalFileName = filePath.substring(filePath.lastIndexOf("/") + 1); // Извлекаем имя файла из пути
-                    System.out.println("Original File Name: " + originalFileName);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    uploadPhoto(petsId, nameFile);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+                } else if (update.message().photo() != null) {
+                    telegramCustomer = update.message().from();
+                    nameCustomer = telegramCustomer.firstName();
+                    Long chatId = update.message().chat().id();
+                    long petsId = customer.findCustomerByChatId(chatId).getPets().getId();
+                    PhotoSize[] photoSizes = update.message().photo();
+                    List<PhotoSize> photos = Arrays.stream(photoSizes).toList();
+                    PhotoSize photoSize = Objects.requireNonNull(photos.stream()
+                            .max(Comparator.comparing(PhotoSize::fileSize))
+                            .orElse(null));
+                    String f_id = photoSize.fileId();
+                    String nameFile = LocalDate.now() + " Фото питомца с id = " + petsId
+                            + ".";
+                    sendMessages.sendMessage(1284536796L, "Клиент " + nameCustomer +
+                            " id = "
+                            + telegramCustomer.id()
+                            + " прислал фото " + nameFile
+                    );
+                    File file = getFile(f_id);
+                    InputStream photoInputStream;
+                    try {
+                        photoInputStream = new URL("https://api.telegram.org/file/bot"
+                                + telegramBot.getToken() + "/" + file.getPath()).openStream();
+                        Files.copy(photoInputStream, Paths.get(nameFile));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    String localPath = "C:\\Users\\777\\IdeaProjects\\telegram_chat_zooShelter\\photo_pet";
+                    Path localFilePath = Paths.get(localPath, nameFile);
+                    File localFile = localFilePath.toFile();
+                    MultipartFile multipartFile;
+                    try {
+                        multipartFile = new MockMultipartFile("file", nameFile, "image/jpeg", photoInputStream);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try (OutputStream os = new FileOutputStream(localFile)) {
+                        os.write(multipartFile.getBytes());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             } else if (update.callbackQuery() != null) {
-                Long chatId = update.callbackQuery().message().chat().id();
+               Long chatId = update.callbackQuery().message().chat().id();
                 switch (update.callbackQuery().data()) {
                     case ("CAT"):
                         responseOnCommand.responseOnCommandShelter(chatId, greetingTextCat, "кошек");
@@ -220,25 +223,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
-
-    private MultipartFile createMultipartFileFromPhotoSize() throws IOException {
-        // Сохраняем фото на сервере
-        String filePath = coversDir + nameFile;
-        try {
-            File file = new File(filePath);
-            // Чтение содержимого файла
-            InputStream inputStream = Files.newInputStream(Paths.get(filePath));
-            String originalFilename = file.getName();
-            // Создаем объект MultipartFile из сохраненного файла
-            return new MockMultipartFile(originalFilename, inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    private void uploadPhoto(Long petsId, String nameFile ) throws IOException {
-        MultipartFile multipartFile = createMultipartFileFromPhotoSize(nameFile);
-        photoPetService.uploadPhotoPet(petsId, multipartFile);
-    }
-
-
 }
+
+
+
